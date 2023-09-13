1a59e9dba56c5ab8060ff2d93d19309690d881bc
Ayush Sarodey <72145248+AyushSarode@users.noreply.github.com>
2023-09-13 12:13:59 +0530

    Update Fibonacci.java

--- Conditional And Loops/Fibonacci.java
+++ Conditional And Loops/Fibonacci.java
@@ -1,6 +1,6 @@
 package com.Ayush;
 
-// Fibonacci Number :- The Fibonacci sequence is a type series where each number is the sum of the two that precede it. It starts from 0 and 1 usually. The Fibonacci sequence is given by 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, and so on.
+// Fibonacci Number :- The Fibonacci sequence is a type series where each number is the sum of the two that precede it. It starts from 0 and 1 usually. The Fibonacci sequence is given by 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, and so on. By adding 0 and 1, we get the third number as 1. Then by adding the second and the third number (i.e) 1 and 1, we get the fourth number as 2, and similarly, the process goes on. Thus, we get the Fibonacci series as 0, 1, 1, 2, 3, 5, 8, ……. Hence, the obtained series is called the Fibonacci number series.
 
 import java.util.Scanner;
 
