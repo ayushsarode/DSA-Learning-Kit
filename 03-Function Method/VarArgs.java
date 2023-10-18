912d0566d8b862659639be9071a8b78a47bcd529
Ayush Sarodey <72145248+ayushsarode@users.noreply.github.com>
2023-10-18 16:37:41 +0530

    Update VarArgs.java

--- 03-Function Method/VarArgs.java
+++ 03-Function Method/VarArgs.java
@@ -1,1 +1,30 @@
+package com.ayush;
+
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        // The main method is the entry point of the program.
+        // It's where the execution starts.
+
+//       fun(4,5,2,7,4,8); // This line is commented out.
+
+       // Calling the 'multiple' method with some arguments.
+       multiple(2, 3, "ayush", "ash", "peter");
+    }
+
+    // The 'multiple' method takes an integer 'a', an integer 'b', and varargs 'v' (a variable number of Strings).
+    static void multiple(int a, int b, String ...v){
+        // Printing the elements of the 'v' array.
+        System.out.println(Arrays.toString(v));
+    }
+
+    // The 'fun' method is commented out and not in use.
+    // It would take a variable number of integers and print them if it were called.
+//    static void fun(int ...a){
+//
+//        System.out.println(Arrays.toString(a));
+//    }
+
+}
 
