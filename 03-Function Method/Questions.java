82b160e359b9d0c88321596054fe25ef64b26cf8
Ayush Sarodey <72145248+ayushsarode@users.noreply.github.com>
2023-10-20 17:11:59 +0530

    Update Questions.java

--- 03-Function Method/Questions.java
+++ 03-Function Method/Questions.java
@@ -6,7 +6,31 @@ public class Questions {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        boolean ans = isPrime(n);
+        System.out.println(ans);
+
+        System.out.println(
+                isArmstrong((n)) );
     }
+
+    // print all the 3 digits armstrong numbers
+    static boolean isArmstrong(int n) {
+        int original = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int rem = n % 10;
+            n = n / 10;
+            sum = sum + rem*rem*rem;
+        }
+
+        if (sum == original) {
+            return true;
+        }
+        return false;
+
+    }
+
 
     static boolean isPrime(int n) {
         if (n <= 1) {
