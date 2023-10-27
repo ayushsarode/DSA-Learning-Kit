e8ed95a6ddd51af8babfaa241fccb0c0401b866b
Ayush Sarodey <72145248+ayushsarode@users.noreply.github.com>
2023-10-27 20:08:14 +0530

    Update BasicsOfArrays.java

--- Arrays/BasicsOfArrays.java
+++ Arrays/BasicsOfArrays.java
@@ -9,16 +9,20 @@ public class BasicsOfArrays {
         // SYNTAX:
         // datatype[] variable_name = new datatype[size];
 
-        // Q: store 5 names
+        // Q: store 5 names (STRING ARRAY)
        //  String[] name = {"ayush", "rohit", "virat", "aditya", "ashwin"};
         // System.out.println(name[1]);
 
-        // Store 5 ID
+        // Store 5 ID (INTEGER ARRAY)
         // int[] ID = {01, 23, 35, 25, 25};
 
-         int[] RollNo; //declaration of array, RollNo is getting defined in the stack
-         RollNo = new int[5]; //initialisation: actually here object is being created in the memory (heap)
+        // int[] RollNo; //declaration of array, RollNo is getting defined in the stack
+        // RollNo = new int[5]; //initialisation: actually here object is being created in the memory (heap)
 
+        // System.out.println(RollNo[3]); // by default 0 value
+
+        String[] arr = new String[4];
+        System.out.println(arr[0]); // output: null; null is empty object
 
     }
 }
