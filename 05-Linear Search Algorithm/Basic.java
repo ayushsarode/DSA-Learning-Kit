46c3afacc7ef6ce60593a254fa1ec729cff92463
Ayush Sarodey <72145248+ayushsarode@users.noreply.github.com>
2023-11-05 15:47:22 +0530

    Update Basic.java

--- 05-Linear Search Algorithm/Basic.java
+++ 05-Linear Search Algorithm/Basic.java
@@ -1,1 +1,31 @@
+package com.ayush;
+
+public class Basics {
+    public static void main(String[] args) {
+        int[] nums = {24,33,5,3,19,3,8,36,48,36,8,45};
+        int target = 8;
+        int ans = LinearSearch(nums, target);
+        System.out.println(ans + "th index");
+    }
+
+    // search in the array: return the index if item found
+    // otherwise if item not found return -1
+    static int LinearSearch(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+        // run a for loop
+        for (int index = 0; index < arr.length; index++) {
+            //check for elemnet at every index if it is = target
+            int element = arr[index];
+            if (element == target) {
+                return index;
+            }
+        }
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+            return -1;
+    } 
+}
+
 
