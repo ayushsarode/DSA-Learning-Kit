67cbca06291ea69ba952bebdadead4ea82268bb1
Ayush Sarode <72145248+AyushSarode@users.noreply.github.com>
2024-07-01 10:06:31 +0530

    evendigits

--- 05-Linear Search Algorithm/EvenDigits.java
+++ 05-Linear Search Algorithm/EvenDigits.java
@@ -3,17 +3,17 @@ package com.ayush;
 public class EvenDigits {
     public static void main(String[] args) {
         int[] nums = {12, 345,2,6,7896};
-    //    System.out.println(findNumber(nums));
-        System.out.println(digits((54637)));
+        System.out.println(findNumber(nums));
+//        System.out.println(digits((54637)));
     }
     static int findNumber(int[] nums) {
-        int count = 0;
-        for (int num : nums) {
-            if (even(num)) {
-                count++;
-            }
+int count = 0;
+for (int num :nums) {
+    if (even(num)){
+        count ++;
+    }
         }
-        return count;
+return count;
     }
 
     //function to check whether a number contains even digits or not
