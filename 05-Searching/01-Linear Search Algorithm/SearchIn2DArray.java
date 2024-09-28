c3f4493a75919ab1e6a9562120dc7a7873832567
Ayush Sarode <72145248+AyushSarode@users.noreply.github.com>
2024-09-28 09:57:03 +0530

    folder structure

---
+++ 05-Searching/01-Linear Search Algorithm/SearchIn2DArray.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class SearchIn2DArray {
+    public static void main(String[] args) {
+        int [] [] arr = {
+                {4, 25, 2},
+                {35, 63, 58, 28},
+                {46, 88, 36,12},
+                {74, 15}
+
+        };
+        int target = 88;
+        int[] ans = search(arr, target);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] search(int[][] arr, int target) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] == target) {
+                    return new int[]{row, col};
+
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    
+}
