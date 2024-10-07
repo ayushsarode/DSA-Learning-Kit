db477f3e8b8e07228db5e9945a937ba8c7ce2524
Ayush Sarode <72145248+AyushSarode@users.noreply.github.com>
2024-10-07 17:33:47 +0530

    cyclic Qs

---
+++ 06-Sorting/04-CyclicSort/DisappearedNum/DisappearedNum.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisappearedNum {
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 2, 7, 8, 2, 3, 1};
+        System.out.println(findDisappearedNumbers(arr));
+    }
+
+    public static List<Integer> findDisappearedNumbers(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] <= arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1) {
+                ans.add(index + 1);
+            }
+        }
+        return ans;
+    }
+
+    public static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
---
+++ 06-Sorting/04-CyclicSort/MissingNum/Main.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/missing-number/
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {4, 0, 2, 1}; // Fixed missing semicolon
+        System.out.println(missingNumber(arr));
+    }
+
+    public static int missingNumber(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i];
+            // Ensure correct is within bounds and that the current element is not in the right place
+            if (correct < arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+        // Find the first index that does not have the correct number
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index) {
+                return index; // Return the missing number
+            }
+        }
+
+        return arr.length; // If all numbers are in place, the missing number is n
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
