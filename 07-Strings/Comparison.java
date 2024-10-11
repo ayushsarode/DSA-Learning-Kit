8ce73e62bc73d671c1040d593d7faad0e63a2dd7
Ayush Sarode <72145248+AyushSarode@users.noreply.github.com>
2024-10-11 19:28:05 +0530

    cyclic Qs added

---
+++ 07-Strings/Comparison.java
@@ -0,0 +1,27 @@
+public class Comparison {
+
+    public static void main(String[] args) {
+
+        String name1 = "ayush";
+        String name2= "ayush";
+
+        System.out.println(name1 == name2);
+
+
+        ///creating diff object of same value 
+        String a = new String("john");
+        String b = new String("john");
+        String c = a;
+        // System.out.println(a == b);
+        //same 
+        System.out.println(c == b);
+
+
+        // in this it doesn't care about ref var is pointing towards same object or not, it c=just car about the same values
+        System.out.println(a.equals(b));
+
+
+        //index for the string
+        System.out.println(name1.charAt(0));
+    }
+}
