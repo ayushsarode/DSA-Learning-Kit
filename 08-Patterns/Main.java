4f855b70d563512ddad2c6469948bbbdc6b0410f
Ayush Sarode <72145248+AyushSarode@users.noreply.github.com>
2024-10-21 21:26:36 +0530

    patterns

--- 08-Patterns/Main.java
+++ 08-Patterns/Main.java
@@ -4,6 +4,8 @@ public class Main {
         pattern1(4);
         pattern2(5);
         pattern3(5);
+        pattern4(4);
+        pattern5(5);
     }
 
     static void pattern1(int n) {
@@ -38,5 +40,25 @@ public class Main {
             System.out.println();
         }
         System.out.println();
+    }
+
+    static void pattern4(int n) {
+        for(int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    static void pattern5(int n) {
+        for (int row = 1; row < 2 * n; row++) {
+            int totalColsInRow = row > n ? 2  * n - row : row;
+            for (int col = 0; col < totalColsInRow ; col++) {
+                 System.out.print("* ");
+            }
+            System.out.println();
+        }  
     }
 }
